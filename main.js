23acc049cbb17ba7df8afd8162e36da96b881ff9
Liel <liel.izcha321@gmail.com>
2021-08-13 19:26:28 +0300

    first commit

---
+++ main.js
@@ -0,0 +1,25 @@
+function minus(num1, num2) {
+    var sum = num1 - num2
+    return sum
+}
+
+console.log(minus(4, 2));
+
+function multi(num1, num2) {
+    sum = num1 * num2
+    return sum
+
+}
+console.log(multi(4, 2));
+function chiluk(num1, num2) {
+    sum = num1 / num2
+    return sum
+}
+console.log(chiluk(4, 2));
+function plus(num1, num2) {
+
+    sum = num1 + num2
+    return sum
+}
+plus(4, 2)
+console.log(plus(2, 4));
