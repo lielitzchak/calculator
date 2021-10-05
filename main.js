c19163424e53deb744ea7bd759b5d04e97f8d401
Liel <liel.izcha321@gmail.com>
2021-10-05 20:40:26 +0300

    calculator

--- main.js
+++ main.js
@@ -1,34 +1,99 @@
-const inputNum1 = document.getElementById("inputNum1");
-const inputNum2 = document.getElementById("inputNum2");
-const btnInput = document.getElementById("btnInput");
-const plusInput = document.getElementById("plus");
-const minInput = document.getElementById("min");
-const dublInput = document.getElementById("dubele");
-const divisionInput = document.getElementById("division");
-const numbers = document.getElementsByClassName("numbers");
+// const inputNum1 = document.getElementById("inputNum1");
+// const inputNum2 = document.getElementById("inputNum2");
+// const btnInput = document.getElementById("btnInput");
+// const plusInput = document.getElementById("plus");
+// const minInput = document.getElementById("min");
+// const dublInput = document.getElementById("dubele");
+// const divisionInput = document.getElementById("division");
+// const numbers = document.getElementsByClassName("numbers");
+// for (let i = 0; i < numbers.length; i++) {
+//   numbers[i].addEventListener("click", function () {
+//     inputNum1.value += numbers[i].innerText;
+//   });
+// }
 
-for (let i = 0; i < numbers.length; i++) {
-  numbers[i].addEventListener("click", function () {
-    inputNum1.value += numbers[i].innerText;
-  });
+// plusInput.addEventListener("click", function () {
+//   let sum = +inputNum1.value + +inputNum2.value;
+//   console.log(sum);
+// });
+
+// minInput.addEventListener("click", function () {
+//   let sum = +inputNum1.value - +inputNum2.value;
+//   console.log(sum);
+// });
+
+// dublInput.addEventListener("click", function () {
+//   let sum = +inputNum1.value * +inputNum2.value;
+//   console.log(sum);
+// });
+
+// divisionInput.addEventListener("click", function () {
+//   let sum = +inputNum1.value / +inputNum2.value;
+//   console.log(sum);
+// });
+
+//! 04.10.2021
+
+let cleanAll = document.getElementById("cleanAll");
+let Solution = document.getElementById("Solution");
+let opers = document.getElementsByClassName("oper");
+let num;
+let operator;
+let shouldClear = false;
+let Equal = document.getElementById("Equal");
+let clean = document.getElementById("clean");
+
+//? Start
+let btnNumbers = document.getElementsByClassName("numbers");
+
+for (let i = 0; i < btnNumbers.length; i++) {
+  btnNumbers[i].onclick = () => concat_number(btnNumbers[i].value);
+}
+for (let i = 0; i < opers.length; i++) {
+  opers[i].onclick = () => {
+    // opers[i].style.color = "red";
+    num = Number(Solution.innerText);
+    operator = opers[i].value;
+    shouldClear = true;
+  };
+}
+function concat_number(num) {
+  if (shouldClear == true) {
+    Solution.innerText = "";
+    shouldClear = false;
+  }
+  Solution.innerText += num;
 }
 
-plusInput.addEventListener("click", function () {
-  let sum = +inputNum1.value + +inputNum2.value;
-  console.log(sum);
-});
+Equal.onclick = () => {
+  Solution.innerText = cal(num, Number(Solution.innerText), operator);
+};
 
-minInput.addEventListener("click", function () {
-  let sum = +inputNum1.value - +inputNum2.value;
-  console.log(sum);
-});
+function cal(num1, num2, op) {
+  switch (op) {
+    case "+":
+      return num1 + num2;
 
-dublInput.addEventListener("click", function () {
-  let sum = +inputNum1.value * +inputNum2.value;
-  console.log(sum);
-});
+    case "-":
+      return num1 - num2;
 
-divisionInput.addEventListener("click", function () {
-  let sum = +inputNum1.value / +inputNum2.value;
-  console.log(sum);
-});
+    case "*":
+      return num1 * num2;
+
+    case "/":
+      return num1 / num2;
+  }
+}
+
+//!\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
+
+cleanAll.onclick = () => {
+  num = 0;
+  Solution.innerText = "";
+};
+clean.onclick = () => {
+  Solution.innerText = Solution.innerText.substring(
+    0,
+    Solution.innerText.length - 1
+  );
+};
